8ab2a97a87badd48ba744d23df73e120b10d0a11
Amiel Bryan Gaudia <abgaudia@veteranmedicalgroupinc.com>
2026-04-23 06:52:12 +0800

    Update app configuration for production environment and improve token expiration handling

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,9 +1,18 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const app_config = {
-    domain: 'http://localhost:8200',
-    
-    accessTokenExp: new Date(Date.now() + 15 * 60 * 1000), // 15 minutes from now
-    refreshTokenExp: new Date(Date.now() + 20 * 60 * 60 * 1000), // 20 hours from now
+    domain: isProduction ? 'yourdomain.com' : 'localhost',
+
+
+    // computed properties for the access and refresh token expiration dates
+    get accessTokenExp() { return new Date(Date.now() + 15 * 60 * 1000); }, // 15 minutes from now
+    get refreshTokenExp() { return new Date(Date.now() + 20 * 60 * 60 * 1000); }, // 20 hours from now
 
     actoCookie: `acto_${process.env.NODE_ENV}`,
     retoCookie: `reto_${process.env.NODE_ENV}`,
+
+    cookieOptions: {
+        sameSite: (isProduction ? 'none' : 'lax') as 'none' | 'lax',
+        secure: isProduction,
+    },
 };
