7cb708924b65b2e7bf33ed24efc08c823e13d1c5
Amiel Bryan Gaudia <abgaudia@veteranmedicalgroupinc.com>
2026-04-29 04:14:07 +0800

    Update Mongoose configuration to use environment variables for database URIs, enhancing flexibility for local and Docker environments.

--- src/configs/mongoose.configs.ts
+++ src/configs/mongoose.configs.ts
@@ -2,9 +2,11 @@ import mongoose from 'mongoose';
 import { TodoModel } from '../model/todo-model';
 import { UserModel } from '../model/user-model';
 
-const MONGO_URI = 'mongodb://root:root@mongodb:27017/db_api_training?authSource=admin';
+// const MONGO_URI_DOCKER = 'mongodb://root:root@mongodb:27017/db_api_training?authSource=admin';
+const MONGO_URI_DOCKER = process.env.MONGO_URI_DOCKER!;
+const MONGO_URI_LOCAL = process.env.MONGO_URI_LOCAL!;
 const models = [TodoModel, UserModel];
 export const connectMongoose = async (): Promise<void> => {
-    await mongoose.connect(MONGO_URI);
-    await Promise.all(models.map((model) => model.syncIndexes())); //avoid single char variables
+    await mongoose.connect(MONGO_URI_LOCAL);
+    await Promise.all(models.map((model) => model.syncIndexes()));
 };
